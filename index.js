b0359f2b9998eb5a92fe9b0f355f4f64248293b1
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-08-01 22:27:24 +0000

    attempting to put the player that gets the most sixes gets 2 extra points. unsuccessful at the momnet

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dice = [ 1, 2, 3, 4, 5, 6];
 var pointPlayer1 = 0
 var pointPlayer2 = 0
+var player1GetsSix = 0
+var player2GetsSix = 0
 play();
 
 function play(){
@@ -10,7 +12,7 @@ function play(){
 
 diceQuestion1();
 function diceQuestion1(){
-  console.log('Player1 points ' + pointPlayer1)
+  // console.log('Player1 points ' + pointPlayer1)
 var play = prompt('player1 would you like to roll the dice?')
   if (play == 'yes'){
   roll();
@@ -27,13 +29,18 @@ function roll(){
   // let dice1 = Math.floor(Math.random() * 6 + 1)
  const random = Math.floor(Math.random() * dice.length);
 //  console.log(random, dice[random]);
-  console.log('you rolled a ' + dice[random])
+  console.log('Player 1 rolled a ' + dice[random])
   if(dice[random] == 2){
     console.log('double points')
+    pointPlayer2 = pointPlayer2 * 2
      if (pointPlayer1 == 0){
        pointPlayer1 ++
        pointPlayer1 = pointPlayer1 * 2}
-    }else{
+    } else if (dice[random] == 6){
+      player1GetsSix ++
+      pointPlayer1 = pointPlayer1 + dice[random];
+    }
+    else{
       pointPlayer1 = pointPlayer1 + dice[random];
       }
     }
@@ -41,7 +48,7 @@ function roll(){
 
 diceQuestion2();
 function diceQuestion2(){
-  console.log('Player2 points ' + pointPlayer2)
+  // console.log('Player2 points ' + pointPlayer2)
 var play = prompt('player2 would you like to roll the dice?')
   if (play == 'yes'){
   roll2();
@@ -58,13 +65,18 @@ function roll2(){
   // let dice2 = Math.floor(Math.random() * 6 + 1)
   const random = Math.floor(Math.random() * dice.length);
 // console.log(random, dice[random]);
-  console.log('you rolled a ' + dice[random])
+  console.log('Player 2 rolled a ' + dice[random])
   if(dice[random] == 2){
     console.log('double points')
+    pointPlayer2 = pointPlayer2 * 2
     if(pointPlayer2 == 0){
     pointPlayer1 ++
     pointPlayer2 = pointPlayer2 * 2}
-  }else{
+  } else if (player2GetsSix == 6){
+    player2GetsSix ++
+    pointPlayer2 = pointPlayer2 + dice[random];
+  }
+  else{
     pointPlayer2 = pointPlayer2 + dice[random];
   }
 }
@@ -77,14 +89,25 @@ ending();
   }
 
 function ending(){
+  if(player1GetsSix > player2GetsSix){
+    pointPlayer1 + 2
+    } else {
+      pointPlayer2 + 2
+    }
 if( pointPlayer1 > pointPlayer2 ){
   console.log('player1 wins')
   console.log('thanks for playing')
+  console.log('Player1 points ' + pointPlayer1)
+  console.log('Player2 points ' + pointPlayer2)
 }else if(pointPlayer1 == pointPlayer2){
   console.log('draw')
   console.log('thanks for playing')
+  console.log('Player1 points ' + pointPlayer1)
+  console.log('Player2 points ' + pointPlayer2)
 }else{
   console.log('player2 wins')
   console.log('thanks for playing')
+  console.log('Player1 points ' + pointPlayer1)
+  console.log('Player2 points ' + pointPlayer2)
 }}
 }
